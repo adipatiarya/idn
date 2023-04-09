67ee9ae07eee9ab0883474290eeb2f41d16b1c9d
Adipati Arya <adipati@kali.xnodes.my.id>
2023-04-09 14:05:12 +0700

    add module user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
-@Module({ imports: [AuthModule] })
+@Module({ imports: [AuthModule, UserModule] })
 export class AppModule {}
