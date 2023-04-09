45768f8321e41f0d9f47a7ac4dfc38a8fd7d0af5
Adipati Arya <adipati@kali.xnodes.my.id>
2023-04-09 16:52:26 +0700

    global validation

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,11 @@
-export interface AuthDto {
-  email: string;
-  password: string;
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
 }
