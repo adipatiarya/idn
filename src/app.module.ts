c761ad4590c1c86bee4ee70d7d51a81b89df1131
Adipati Arya <adipati@kali.xnodes.my.id>
2023-04-09 14:07:09 +0700

    add module bookmark

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
-@Module({ imports: [AuthModule, UserModule] })
+@Module({ imports: [AuthModule, UserModule, BookmarkModule] })
 export class AppModule {}
