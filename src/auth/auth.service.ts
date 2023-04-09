f8623edc6f148b49232a764d7553c10fa1b3a4b1
Adipati Arya <adipati@kali.xnodes.my.id>
2023-04-09 21:14:05 +0700

    implementasi access token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,9 +2,15 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(dto: AuthDto) {
     try {
       // generate the password hash
@@ -46,7 +52,23 @@ export class AuthService {
     if (!matches) throw new ForbiddenException('Password incorrect');
 
     // send back to user
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
+  }
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret,
+    });
+    return {
+      access_token: token,
+    };
   }
 }
