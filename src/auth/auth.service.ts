71a7c0914a73fa829792567d9454703db1219394
Adipati Arya <adipati@kali.xnodes.my.id>
2023-04-09 18:25:06 +0700

    handle error email duplicate

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
-    // generate the password hash
-    const hash = await argon.hash(dto.password);
-    // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-        firstName: dto.firstName,
-        lastName: dto.lastName,
-      },
-    });
-    delete user.hash;
+    try {
+      // generate the password hash
+      const hash = await argon.hash(dto.password);
+      // save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+        },
+      });
+      delete user.hash;
 
-    // return the saved user
-    return user;
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Invalid credentials');
+      }
+      throw error;
+    }
   }
 
   signin() {
